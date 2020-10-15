e60cde7a6b8be93792c804489696150f1c9dd9f2
Rogers Ramirez <rogers.ramirez2008@gmail.com>
2020-10-15 18:11:46 -0400

    linked the front end fies and the server

--- server.js
+++ server.js
@@ -115,8 +115,17 @@ app.get('/api/animals/:id', (req, res) => {
   }
 });
 
+//routes for other sides
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
 });
 
 app.post('/api/animals', (req, res) => {
